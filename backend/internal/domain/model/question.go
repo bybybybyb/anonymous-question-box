74df2b808a05c615f9c6cb42a6ee2686e6831f60
BybYb_bYByB <2519878+bybybybyb@users.noreply.github.com>
2021-07-23 00:48:28 -0400

    refine question model

--- backend/internal/domain/model/question.go
+++ backend/internal/domain/model/question.go
@@ -7,11 +7,10 @@ import (
 type Question struct {
 	ID         int32     `json:"-"`
 	UUID       string    `json:"-"`
-	Type       string    `json:"question_type,omitempty"`
-	Owner      string    `json:"question_owner"`
-	Text       string    `json:"question"`
+	Type       string    `json:"type,omitempty"`
+	Owner      string    `json:"owner"`
+	Text       string    `json:"text"`
 	AskedAt    time.Time `json:"asked_at"`
 	AnswerText string    `json:"answer,omitempty"`
 	AnsweredAt time.Time `json:"answered_at,omitempty"`
-	Deleted    bool      `json:"-"`
 }
