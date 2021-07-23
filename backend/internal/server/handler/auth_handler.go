593e95b0617f50e97644cc149adc40760b71a935
BybYb_bYByB <ylns1314@gmail.com>
2021-07-23 19:42:45 -0400

    status code change for auth handler

--- backend/internal/server/handler/auth_handler.go
+++ backend/internal/server/handler/auth_handler.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/anonymous-question-box/internal/domain/repository"
 	"github.com/gin-gonic/gin"
+	"net/http"
 	"strings"
 )
 
@@ -17,12 +18,12 @@ func (a *AuthHandler) Authenticate(c *gin.Context) {
 	if len(splits) == 2 {
 		token = splits[1]
 	} else {
-		c.AbortWithStatusJSON(403, ErrorResp{Error: "无效token"})
+		c.AbortWithStatusJSON(http.StatusForbidden, ErrorResp{Error: "无效token"})
 		return
 	}
 	uuid, isAdmin, err := a.TokenManager.ValidateToken(c, token)
 	if err != nil {
-		c.AbortWithStatusJSON(403, ErrorResp{Error: fmt.Sprintf("无法解析token，错误信息：%s", err.Error())})
+		c.AbortWithStatusJSON(http.StatusUnauthorized, ErrorResp{Error: fmt.Sprintf("无法解析token，错误信息：%s", err.Error())})
 		return
 	}
 	c.Set("is_admin", isAdmin)
@@ -31,13 +32,13 @@ func (a *AuthHandler) Authenticate(c *gin.Context) {
 
 func (a *AuthHandler) AuthorizeOwner(c *gin.Context) {
 	if !c.GetBool("is_admin") {
-		c.AbortWithStatusJSON(403, ErrorResp{Error: "未授权访问"})
+		c.AbortWithStatusJSON(http.StatusUnauthorized, ErrorResp{Error: "未授权访问"})
 	}
 }
 
 func (a *AuthHandler) BlockOwner(c *gin.Context) {
 	if c.GetBool("is_admin") {
-		c.AbortWithStatusJSON(403, ErrorResp{Error: "提问箱主人能问自己和其他提问箱主人问题嘛？答案是不能"})
+		c.AbortWithStatusJSON(http.StatusForbidden, ErrorResp{Error: "提问箱主人能问自己和其他提问箱主人问题嘛？答案是不能"})
 		return
 	}
 }
