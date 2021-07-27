20ee5117b0092442e05f06904a065a57a9669a40
BybYb_bYByB <2519878+bybybybyb@users.noreply.github.com>
2021-07-27 16:23:14 -0400

    pprof

--- backend/main.go
+++ backend/main.go
@@ -5,13 +5,14 @@ import (
 	"fmt"
 	"github.com/anonymous-question-box/internal/infrastructure"
 	"github.com/anonymous-question-box/internal/server"
+	"github.com/gin-contrib/pprof"
 	"github.com/spf13/viper"
 )
 
 func main() {
-	configFlag := flag.String("config", "../config/config.yaml", "path to the config file")
+	configFlag := flag.String("config", "./config/config.yaml", "path to the config yaml")
 
-	// configs
+	// server config
 	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
 	viper.SetConfigFile(*configFlag)
@@ -19,17 +20,24 @@ func main() {
 	// default configs
 	viper.SetDefault("host", "")
 	viper.SetDefault("port", "8080")
-	// 默认问题自动过期时间
-	viper.SetDefault("question_expiration_days", 60)
 	// 默认字数限制
 	viper.SetDefault("default_rune_limit", 500)
+
 	// inits
 	err := infrastructure.InitSQLiteDB()
 	if err != nil {
 		panic(err.Error())
 	}
-
+	err = infrastructure.LoadProfiles()
+	if err != nil {
+		panic(err.Error())
+	}
+	r, err := server.SetupRoutes()
+	if err != nil {
+		panic(err.Error())
+	}
+	// profiling
+	pprof.Register(r)
 	// start server
-	r := server.SetupRoutes()
 	r.Run(fmt.Sprintf("%s:%s", viper.GetString("host"), viper.GetString("port")))
 }
