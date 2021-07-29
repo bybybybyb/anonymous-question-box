1b6efb898723cefe0a88b9ce160fff36a1601e54
BybYb_bYByB <ylns1314@gmail.com>
2021-07-29 08:34:43 -0400

    always return answer in question model

--- backend/internal/domain/model/question.go
+++ backend/internal/domain/model/question.go
@@ -7,11 +7,11 @@ import (
 type Question struct {
 	ID         int32     `json:"-"`
 	UUID       string    `json:"uuid"`
-	Type       string    `json:"type,omitempty"`
+	Type       string    `json:"type"`
 	Owner      string    `json:"owner"`
 	Text       string    `json:"text"`
 	WordCount  int32     `json:"word_count"`
 	AskedAt    time.Time `json:"asked_at"`
-	AnswerText string    `json:"answer,omitempty"`
-	AnsweredAt time.Time `json:"answered_at,omitempty"`
+	AnswerText string    `json:"answer"`
+	AnsweredAt time.Time `json:"answered_at"`
 }
