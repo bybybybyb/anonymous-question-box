0293a3649abe8ac043ac958990a9917089b4b375
BybYb_bYByB <2519878+bybybybyb@users.noreply.github.com>
2021-08-01 01:35:59 -0400

    change token to a link

--- frontend/src/components/QuestionSubmission.vue
+++ frontend/src/components/QuestionSubmission.vue
@@ -1,30 +1,34 @@
 <template>
-  <Header :hideBackBtn="true"></Header>
-  <div class="container">
-    <div class="card my-3">
-      <div class="card-body">
-        <h1>感谢投稿！</h1>
-        <div class="contaienr">
-          <p class="mx-3">
-            请妥善保存下方二维码或神秘代码，这将是你找回投稿的唯一方式！
-          </p>
-          <p class="mx-3">记得时不时来看一眼有没收到回复哦~</p>
-          <qrcode-vue
-            class="col-6 my-4"
-            :value="question_url"
-            :size="200"
-          ></qrcode-vue>
-          <p>神秘代码：</p>
-          <div class="col-md-12">
-            <p style="line-break: anywhere">{{ $route.query.token }}</p>
+  <div>
+    <Header :hideBackBtn="true"></Header>
+    <div class="container">
+      <div class="card my-3">
+        <div class="card-body">
+          <h1>感谢投稿！</h1>
+          <div class="contaienr">
+            <p class="mx-3">
+              请妥善保存下方二维码和链接，这将是你找回投稿的唯一方式！
+            </p>
+            <p class="mx-3">记得时不时来看一眼有没收到回复哦~</p>
+            <qrcode-vue
+              class="col-6 my-4"
+              :value="question_url"
+              :size="200"
+            ></qrcode-vue>
+            <p>链接直达（包含神秘代码）：</p>
+            <div class="col-md-12">
+              <a :href="question_url" style="line-break: anywhere">{{
+                question_url
+              }}</a>
+            </div>
+            <button
+              class="col-6 btn btn-outline-info col-sm-2"
+              v-on:click="goToQuestion"
+            >
+              查看刚提交的投稿
+            </button>
+            <h5 class="m-3">想要继续投稿？请点击返回主页重新开始！</h5>
           </div>
-          <button
-            class="col-6 btn btn-outline-info col-sm-2"
-            v-on:click="goToQuestion"
-          >
-            查看刚提交的投稿
-          </button>
-          <h5 class="m-3">想要继续投稿？请点击返回主页重新开始！</h5>
         </div>
       </div>
     </div>
@@ -43,7 +47,7 @@ export default {
   },
   computed: {},
   methods: {
-    goToQuestion(event) {
+    goToQuestion() {
       this.$router.push({
         name: "question",
         query: { token: this.token },
