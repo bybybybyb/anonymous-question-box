2b955668a1a4488b9d9d17db74bd78b045a04a05
BybYb_bYByB <2519878+bybybybyb@users.noreply.github.com>
2021-08-04 05:35:47 -0400

    remove token completely from main page

--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -34,33 +34,6 @@
           </div>
         </div>
       </div>
-      <div class="row">
-        <div class="col-12">
-          <div class="card m-3 p-3">
-            <div class="row m-3">
-              <h4>查询之前的投稿？</h4>
-            </div>
-            <form class="row">
-              <div class="col-12">
-                <input
-                  v-model="token"
-                  placeholder="请输入神秘代码"
-                  v-on:keyup="makeSubmitClickable"
-                />
-              </div>
-              <div class="col-12">
-                <button
-                  type="button"
-                  :class="[submitBtnStyleClasses, submitBtnActiveClass]"
-                  v-on:click="checkToken"
-                >
-                  提交
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
     </div>
   </div>
 </template>
@@ -77,59 +50,14 @@ export default {
         "border-color": this.ownerProfiles[owner].color_theme.primary_color,
       };
     },
-    makeSubmitClickable() {
-      this.token.length > 0
-        ? (this.submitBtnActiveClass = "")
-        : (this.submitBtnActiveClass = "disabled");
-    },
-    checkToken() {
-      const authTokenHeader = {
-        headers: { Authorization: `Bearer ${this.token}` },
-      };
-      this.axios
-        .get("/api/owner", authTokenHeader)
-        .then((resp) => {
-          if (resp.data.owner != "") {
-            this.$router.push({
-              name: "owners",
-              params: { owner: resp.data.owner },
-              query: { token: this.token.replace(/\s/g, "") },
-            });
-          } else {
-            console.log("something is wrong");
-          }
-        })
-        .catch((err) => {
-          if (err.response.status === 401) {
-            this.$router.push({
-              name: "question",
-              query: { token: this.token.replace(/\s/g, "") },
-            });
-          } else {
-            alert("提问箱好像坏掉了，请通知管理员前来查看！");
-          }
-        });
-    },
     newQuestion(event) {
-      // TODO: consider moving generating new token logic to the new question page, not here
-      this.axios.get("/api/new").then((resp) => {
-        this.$router.push({
-          name: "question-new",
-          query: { token: resp.data.token, owner: event.target.value },
-        });
+      this.$router.push({
+        name: "question-new",
+        params: { owner: event.target.value },
       });
     },
   },
-  mounted() {
-    let uri = window.location.search.substring(1);
-    let params = new URLSearchParams(uri);
-    this.token = params.get("token");
-    if (this.token != null) {
-      this.checkToken();
-    }
-  },
   data: {
-    token: "",
     submitBtnStyleClasses: "my-3 btn btn-outline-primary",
     submitBtnActiveClass: "disabled",
   },
