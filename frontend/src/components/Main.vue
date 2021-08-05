865d549df7fecc27cb154d99fb9d4805589c7deb
BybYb_bYByB <2519878+bybybybyb@users.noreply.github.com>
2021-08-05 19:27:32 -0400

    change logo and favicon

--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -5,28 +5,33 @@
       <div class="row">
         <div class="col-12">
           <div class="card m-3 p-3">
-            <div class="row">
-              <h1>MeUmy的棉花糖</h1>
-            </div>
-            <div class="row">
-              <!-- TODO: refactor here to automatically add buttons by profiles -->
-              <div class="col-6">
-                <button
-                  class="btn btn-outline-info my-2"
-                  :style="setBtnColor('merry')"
-                  v-on:click="newQuestion('merry')"
-                >
-                  咩栗和蜗牛姐姐的棉花糖
-                </button>
+            <div class="card-body m-3">
+              <div class="row">
+                <img src="../assets/marshmallow.svg" alt="" height="200" />
               </div>
-              <div class="col-6">
-                <button
-                  class="btn btn-outline-danger my-2"
-                  :style="setBtnColor('umy')"
-                  v-on:click="newQuestion('umy')"
-                >
-                  呜米和妹妹的棉花糖
-                </button>
+              <div class="row">
+                <h1>MeUmy的棉花糖</h1>
+              </div>
+              <div class="row">
+                <!-- TODO: refactor here to automatically add buttons by profiles -->
+                <div class="col-6">
+                  <button
+                    class="btn btn-outline-info my-2"
+                    :style="setBtnColor('merry')"
+                    v-on:click="newQuestion('merry')"
+                  >
+                    咩栗和蜗牛姐姐的棉花糖
+                  </button>
+                </div>
+                <div class="col-6">
+                  <button
+                    class="btn btn-outline-danger my-2"
+                    :style="setBtnColor('umy')"
+                    v-on:click="newQuestion('umy')"
+                  >
+                    呜米和妹妹的棉花糖
+                  </button>
+                </div>
               </div>
             </div>
           </div>
