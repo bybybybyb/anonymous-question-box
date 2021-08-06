31259a7100b1c4b373f3d82188d9fdf31b93b218
BybYb_bYByB <2519878+bybybybyb@users.noreply.github.com>
2021-08-06 04:50:37 -0400

    background on question page

--- frontend/src/components/QuestionView.vue
+++ frontend/src/components/QuestionView.vue
@@ -3,10 +3,20 @@
     <Header :hideBackBtn="true"></Header>
     <div v-if="just_submitted">
       <QuestionSubmission :token="token" />
-      <QuestionDisplay :token="token" />
+      <QuestionDisplay
+        :question_text="question_text"
+        :asked_at="asked_at"
+        :answer_text="answer_text"
+        :answered_at="answered_at"
+      />
     </div>
     <div v-else>
-      <QuestionDisplay :token="token" />
+      <QuestionDisplay
+        :question_text="question_text"
+        :asked_at="asked_at"
+        :answer_text="answer_text"
+        :answered_at="answered_at"
+      />
       <QuestionSubmission :token="token" />
     </div>
   </div>
@@ -16,6 +26,7 @@
 import Header from "./Header.vue";
 import QuestionDisplay from "./QuestionDisplay.vue";
 import QuestionSubmission from "./QuestionSubmission.vue";
+let currentBgClass = "";
 export default {
   name: "QuestionView",
   props: {
@@ -26,13 +37,51 @@ export default {
     QuestionDisplay,
     QuestionSubmission,
   },
-  created() {
-    console.log(this.just_submitted);
+  beforeCreate() {
     this.token = this.$route.query.token;
+    const authHeader = {
+      headers: { Authorization: `Bearer ${this.token}` },
+    };
+    this.axios
+      .get("/api/questions/question", authHeader)
+      .then((resp) => {
+        this.owner = resp.data.owner;
+        this.type = resp.data.type;
+        this.question_text = resp.data.text;
+        this.asked_at = resp.data.asked_at;
+        this.answer_text = resp.data.answer;
+        this.answered_at = resp.data.answered_at;
+
+        currentBgClass =
+          this.ownerProfiles[this.owner].question_types[this.type].theme
+            .background_class;
+        document.body.classList.remove("bg-light");
+        document.body.classList.add("body-background-" + currentBgClass);
+        if (currentBgClass.includes("dark")) {
+          this.card_background_style = "background: rgba(120,120,120,0.7)";
+        }
+      })
+      .catch((err) => {
+        if (err.response.status === 401) {
+          alert("对不起，未能找到您的投稿。");
+        } else {
+          alert("提问箱好像坏掉了，请通知管理员前来查看！");
+        }
+        this.$router.push({ path: "/" });
+      });
+  },
+  beforeUnmount() {
+    // change back the body background
+    document.body.classList.remove("body-background-" + currentBgClass);
+    document.body.classList.add("bg-light");
   },
   data() {
     return {
       token: "",
+      question_text: "",
+      asked_at: "",
+      answer_text: "",
+      answered_at: "",
     };
   },
 };
