b94d55e3b9f67eda1b5df477ac03943b646666cc
BybYb_bYByB <2519878+bybybybyb@users.noreply.github.com>
2021-08-06 04:06:21 -0400

    profile structure changes

--- backend/internal/domain/model/profile.go
+++ backend/internal/domain/model/profile.go
@@ -2,19 +2,24 @@ package model
 
 type Profile struct {
 	Name          string                  `mapstructure:"name" json:"name"`
-	ColorTheme    ColorTheme              `mapstructure:"color_theme" json:"color_theme"`
+	Colors        Colors                  `mapstructure:"colors" json:"colors"`
 	QuestionTypes map[string]QuestionType `mapstructure:"question_types" json:"question_types"`
 }
 
-type ColorTheme struct {
+type Colors struct {
 	PrimaryColor   string `mapstructure:"primary_color" json:"primary_color"`
 	SecondaryColor string `mapstructure:"secondary_color" json:"secondary_color"`
 }
 
 type QuestionType struct {
+	Name        string `mapstructure:"name" json:"name"`
+	Description string `mapstructure:"description" json:"description"`
+	RuneLimit   int32  `mapstructure:"rune_limit" json:"rune_limit"`
+	Theme       Theme  `mapstructure:"theme" json:"theme"`
+}
+
+type Theme struct {
 	Name            string `mapstructure:"name" json:"name"`
-	Description     string `mapstructure:"description" json:"description"`
-	RuneLimit       int32  `mapstructure:"rune_limit" json:"rune_limit"`
 	BackgroundClass string `mapstructure:"background_class" json:"background_class"`
 }
 
