cb2471ea0c896a70aaca148b19a699e83ea31595
BybYb_bYByB <2519878+bybybybyb@users.noreply.github.com>
2021-08-15 04:21:08 -0400

    support timed question type

--- backend/internal/infrastructure/profile_loader.go
+++ backend/internal/infrastructure/profile_loader.go
@@ -3,19 +3,39 @@ package infrastructure
 import (
 	"github.com/anonymous-question-box/internal/domain/model"
 	"github.com/spf13/viper"
+	"log"
+	"time"
 )
 
-var Profiles map[string]model.Profile
+var Profiles map[string]*model.Profile
 var WebsiteMetadataStore model.WebsiteMetadata
 
 func LoadProfiles() error {
-	Profiles = make(map[string]model.Profile)
-	profiles := []model.Profile{}
+	Profiles = make(map[string]*model.Profile)
+	profiles := []*model.Profile{}
 	err := viper.UnmarshalKey("owner_profiles", &profiles)
 	if err != nil {
 		return err
 	}
 	for _, profile := range profiles {
+		for _, qt := range profile.QuestionTypes {
+			if qt.StartTimeStr != "" {
+				startTime, err := time.Parse(time.RFC3339, qt.StartTimeStr)
+				if err != nil {
+					log.Printf("failed to parse start time from config for owner %s qType %s, %s\n", profile.Name, qt.Name, err.Error())
+					continue
+				}
+				qt.StartTime = startTime
+			}
+			if qt.EndTimeStr != "" {
+				endTime, err := time.Parse(time.RFC3339, qt.EndTimeStr)
+				if err != nil {
+					log.Printf("failed to parse end time from config for owner %s qType %s, %s\n", profile.Name, qt.Name, err.Error())
+					continue
+				}
+				qt.EndTime = endTime
+			}
+		}
 		Profiles[profile.Name] = profile
 	}
 
