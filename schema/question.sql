9f7790aecdd1a2dcd7d554c0d064a5aa42279507
BybYb_bYByB <2519878+bybybybyb@users.noreply.github.com>
2021-09-14 22:40:57 -0400

    schema change for visit status tracking

--- schema/question.sql
+++ schema/question.sql
@@ -4,12 +4,22 @@ CREATE TABLE question (
 	owner TEXT NOT NULL,
 	question_type TEXT NOT NULL,
 	question TEXT NOT NULL,
+	asked_at INTEGER NOT NULL,
 	word_count INTEGER NOT NULL,
 	answer TEXT,
-	asked_at INTEGER NOT NULL,
 	answered_at INTEGER,
+	answered_by TEXT,
 	deleted_at INTEGER
 );
 
-CREATE UNIQUE INDEX uk_uuid ON question (uuid);
+CREATE TABLE visit (
+	id INTEGER PRIMARY KEY,
+	uuid TEXT NOT NULL,
+	last_visited_at INTEGER NOT NULL,
+	visit_count INTEGER NOT NULL,
+	FOREIGN KEY (uuid) REFERENCES question(uuid)
+);
+
+CREATE UNIQUE INDEX uk_question_uuid ON question (uuid);
 CREATE INDEX idx_owner_question_type_asked_at ON question (owner, question_type, asked_at);
+CREATE UNIQUE INDEX uk_visit_uuid ON visit (uuid);
