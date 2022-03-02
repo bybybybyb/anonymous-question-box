5bcf239bc0b0d0c1c17325a3adf6c7e6319cda88
BybYb_bYByB <2519878+bybybybyb@users.noreply.github.com>
2022-03-02 17:37:09 -0500

    frontend package updates

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,15 @@
+import vue from "@vitejs/plugin-vue";
 module.exports = {
-  proxy: {
-    '/api': {
-      target: 'http://localhost:3768',
-      changeOrigin: true,
-      secure: false,
-      ws: true,
-      rewrite: path => path.replace(/^\/api/, '')
-    }
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3768",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
-}
+  plugins: [vue()],
+};
